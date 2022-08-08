f5edc2304ab3d17b2ea18a66a0e0d75e8255c8ea
prajwalc150 <prajwalmech8@gmail.com>
2022-08-08 20:32:25 +0530

    use display: grid

---
+++ app.js
@@ -0,0 +1,38 @@
+const container = document.querySelector('#container');
+n = 10000;
+
+for(i = 0;i<n;i++){
+   let content = document.createElement('div');
+   //content.textContent = `${i}`;
+   content.id = "test";
+   container.appendChild(content);
+}
+const test = document.getElementById("container");
+/*
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+test.addEventListener("mouseenter", (event) => {
+  // highlight the mouseenter target
+  event.target.style.color = "red";
+
+  
+  // reset the color after a short delay
+  setTimeout(() => {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+*/
+// This handler will be executed every time the cursor
+// is moved over a different list item
+test.addEventListener("mouseover", (event) => {
+  // highlight the mouseover target
+  event.target.style.backgroundColor = "orange";})
+
+ /* 
+  // reset the color after a short delay
+  setTimeout(() => {
+    event.target.style.backgroundColor = "";
+  }, 500);
+}, false);
+
+*/
